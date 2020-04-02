e85dfafaa533bbb8ffc94536b94bd178cd842ef8
mi-note <jiangxiewei@qq.com>
2020-04-02 20:21:27 +0800

    1.提供个祖传的gitignore    2.改用guava的loadingCache来做缓存(自带加载阻塞实现),还能追加缓存超时机制.   3.StringBuilder的append没意义,直接用+默认会被jvm优化成StringBuilder

--- apigateway-rpc-genericinvoke/src/main/java/org/dreamlife/hippocampus/apigateway/genericinvoke/service/impl/DubboGenericServiceImpl.java
+++ apigateway-rpc-genericinvoke/src/main/java/org/dreamlife/hippocampus/apigateway/genericinvoke/service/impl/DubboGenericServiceImpl.java
@@ -1,6 +1,9 @@
 package org.dreamlife.hippocampus.apigateway.genericinvoke.service.impl;
 
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.ReferenceConfig;
 import org.apache.dubbo.config.context.ConfigManager;
@@ -10,9 +13,7 @@ import org.dreamlife.hippocampus.apigateway.genericinvoke.model.Response;
 import org.dreamlife.hippocampus.apigateway.genericinvoke.service.RpcGenericService;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.time.Duration;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
@@ -28,77 +29,59 @@ import java.util.concurrent.FutureTask;
 @Slf4j
 @Service
 public class DubboGenericServiceImpl implements RpcGenericService {
-    private final Map<String, FutureTask<GenericService>> cache;
 
+    private final LoadingCache<String, FutureTask<GenericService>> cache;
 
     public DubboGenericServiceImpl() {
-        cache = new ConcurrentHashMap<>();
+        cache = CacheBuilder.newBuilder()
+                .expireAfterWrite(Duration.ofSeconds(60))
+                .build(CacheLoader.from(
+                        (cacheKey) -> new FutureTask<>(() -> {
+                            String[] param = cacheKey.split(".");
+                            ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
+                            reference.setInterface(param[0]);
+                            reference.setGroup(param[1]);
+                            reference.setVersion(param[2]);
+                            reference.setGeneric(true);
+                            reference.setApplication(ConfigManager.getInstance().getApplication().get());
+                            reference.setRegistry(ConfigManager.getInstance().getRegistries().values().iterator().next());
+                            // 考虑部分RPC接口比较耗时
+                            reference.setTimeout(60 * 1000);
+                            // 不进行重试
+                            reference.setRetries(0);
+                            GenericService genericService = reference.get();
+                            return genericService;
+                        })
+                ));
     }
 
     private String generateKey(String interfaceClass, String group, String version) {
-        return new StringBuilder(interfaceClass)
-                .append(".")
-                .append(group)
-                .append(".")
-                .append(version).toString();
-
+        return interfaceClass + "." + group + "." + version;
     }
 
     private GenericService getService(String interfaceClass, String group, String version) throws ExecutionException, InterruptedException {
-        String cacheKey = generateKey(interfaceClass,group,version);
-
+        String cacheKey = generateKey(interfaceClass, group, version);
         FutureTask<GenericService> futureTask = cache.get(cacheKey);
-
-        if(null == futureTask){
-            // 没有命中缓存
-            // 为了解决多程序并发穿透缓存造成的危害，这里设置只允许一个线程去加载结果，其余线程阻塞等待
-            futureTask = new FutureTask(
-                    () ->{
-                        ReferenceConfig<GenericService> reference = new ReferenceConfig<>();
-                        reference.setInterface(interfaceClass);
-                        reference.setVersion(version);
-                        reference.setGeneric(true);
-                        reference.setGroup(group);
-                        reference.setApplication(ConfigManager.getInstance().getApplication().get());
-                        reference.setRegistry(ConfigManager.getInstance().getRegistries().values().iterator().next());
-                        // 考虑部分RPC接口比较耗时
-                        reference.setTimeout(60 * 1000);
-                        // 不进行重试
-                        reference.setRetries(0);
-                        // 根据ReferenceConfig对象获取到GenericService对象的操作很重，需要进行底层通信
-                        // 因此需要缓存创建的GenericService对象
-                        GenericService genericService = reference.get();
-                        return genericService;
-                    }
-            );
-            FutureTask previousTask = cache.putIfAbsent(cacheKey, futureTask);
-            if(null == previousTask){
-                // 写成功的线程执行任务
-                futureTask.run();
-            }else{
-                // 没有写成功的线程便获取写入成功的futureTask
-                futureTask = cache.get(cacheKey);
-            }
-        }
         return futureTask.get();
     }
 
+    @Override
     public Response invoke(GenericInvokeQO genericInvokeQO) {
         GenericService service = null;
-        try{
+        try {
             service = getService(genericInvokeQO.getInterfaceClass(),
                     genericInvokeQO.getGroup(), genericInvokeQO.getVersion());
-        }catch (Exception e){
-            log.warn("RPC服务调用失败: 找不到当前RPC接口的提供者, 请求实体：{}",genericInvokeQO);
-            return Response.fail(400,"RPC服务调用失败: 找不到当前RPC接口的提供者");
+        } catch (Exception e) {
+            log.warn("RPC服务调用失败: 找不到当前RPC接口的提供者, 请求实体：{}", genericInvokeQO);
+            return Response.fail(400, "RPC服务调用失败: 找不到当前RPC接口的提供者");
         }
-        try{
+        try {
             Object result = service.$invoke(genericInvokeQO.getMethodName(),
                     genericInvokeQO.getParameterTypes().toArray(new String[0]), genericInvokeQO.getParameters().toArray());
             return Response.ok(result);
-        }catch (Exception e){
-            log.warn("当前接口调用失败，不存在目标方法，请求实体：{}",genericInvokeQO);
-            return Response.fail(400, String.format("RPC服务调用失败: 不存在目标方法（ %s ）",genericInvokeQO.getMethodName()));
+        } catch (Exception e) {
+            log.warn("当前接口调用失败，不存在目标方法，请求实体：{}", genericInvokeQO);
+            return Response.fail(400, String.format("RPC服务调用失败: 不存在目标方法（ %s ）", genericInvokeQO.getMethodName()));
         }
 
     }
