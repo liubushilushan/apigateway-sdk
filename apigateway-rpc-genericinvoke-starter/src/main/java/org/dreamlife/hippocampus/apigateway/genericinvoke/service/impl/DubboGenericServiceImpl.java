5423ee1ec69a2b60f4112bebc00a52c9b9827d62
yangzai1122 <liujy@ibuscloud.com>
2020-04-07 15:22:09 +0800

    bugfix：泛化调用网关修改applicationConfig和registryConfig的获取方式

--- apigateway-rpc-genericinvoke-starter/src/main/java/org/dreamlife/hippocampus/apigateway/genericinvoke/service/impl/DubboGenericServiceImpl.java
+++ apigateway-rpc-genericinvoke-starter/src/main/java/org/dreamlife/hippocampus/apigateway/genericinvoke/service/impl/DubboGenericServiceImpl.java
@@ -2,8 +2,10 @@ package org.dreamlife.hippocampus.apigateway.genericinvoke.service.impl;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.common.utils.CollectionUtils;
 import org.apache.dubbo.config.ApplicationConfig;
 import org.apache.dubbo.config.ReferenceConfig;
+import org.apache.dubbo.config.RegistryConfig;
 import org.apache.dubbo.config.context.ConfigManager;
 import org.apache.dubbo.config.utils.ReferenceConfigCache;
 import org.apache.dubbo.rpc.service.GenericService;
@@ -13,12 +15,8 @@ import org.dreamlife.hippocampus.apigateway.genericinvoke.service.RpcGenericServ
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
 
 /**
  * Dubbo泛化服务
@@ -33,6 +31,8 @@ import java.util.concurrent.FutureTask;
 @Service
 public class DubboGenericServiceImpl implements RpcGenericService, InitializingBean {
     private final ReferenceConfigCache cache;
+    private ApplicationConfig applicationConfig;
+    private RegistryConfig registryConfig;
 
     public DubboGenericServiceImpl() {
         cache = ReferenceConfigCache.getCache();
@@ -40,6 +40,11 @@ public class DubboGenericServiceImpl implements RpcGenericService, InitializingB
 
     @Override
     public Response invoke(GenericInvokeQO genericInvokeQO) {
+        if(applicationConfig == null || registryConfig == null){
+            log.error("RPC服务调用失败: 缺少dubbo配置");
+            return Response.fail(400, "RPC服务调用失败: 当前服务缺少dubbo配置");
+        }
+
         GenericService service = null;
         ReferenceConfig<GenericService> reference = null;
         try {
@@ -48,17 +53,14 @@ public class DubboGenericServiceImpl implements RpcGenericService, InitializingB
             reference.setVersion(genericInvokeQO.getVersion());
             reference.setGroup(genericInvokeQO.getGroup());
             reference.setGeneric(true);
-            reference.setApplication(ConfigManager.getInstance().getApplication().get());
-            reference.setRegistry(ConfigManager.getInstance().getRegistries().values().iterator().next());
+            reference.setApplication(applicationConfig);
+            reference.setRegistry(registryConfig);
             // 考虑部分RPC接口比较耗时
             reference.setTimeout(60 * 1000);
             // 不进行重试
             reference.setRetries(0);
             service = cache.get(reference);
-        } catch(NoSuchElementException e){
-            log.error("RPC服务调用失败: 缺少dubbo配置");
-            return Response.fail(400, "RPC服务调用失败: 当前服务缺少dubbo配置");
-        } catch(Exception e) {
+        } catch (Exception e) {
             // 需要移除缓存，同时销毁referenceConfig，防止恶意攻击
             cache.destroy(reference);
             log.warn("RPC服务调用失败: 找不到当前RPC接口的提供者, 请求实体：{}", genericInvokeQO);
@@ -79,15 +81,30 @@ public class DubboGenericServiceImpl implements RpcGenericService, InitializingB
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        ConfigManager instance = ConfigManager.getInstance();
-        try{
-            instance.getApplication().get();
-        }catch (NoSuchElementException e){
-            log.error("缺少配置： dubbo.application.name");
+        ApplicationConfig application =  ConfigManager.getInstance().getApplication()
+                .orElseGet(() -> {
+                    ApplicationConfig applicationConfig = new ApplicationConfig();
+                    applicationConfig.refresh();
+                    return applicationConfig;
+                });
+        if(application.isValid()){
+           this.applicationConfig = application;
+        }else{
+            log.error("缺少配置： dubbo.application.name, 这将会导致泛化调用失败");
         }
-        if(instance.getRegistries().isEmpty()){
-            log.error("缺少配置： dubbo.registry.address");
-        }
+        List<RegistryConfig> registryConfigs = ConfigManager.getInstance().getDefaultRegistries()
+                .filter(CollectionUtils::isNotEmpty)
+                .orElseGet(() -> {
+                    RegistryConfig registryConfig = new RegistryConfig();
+                    registryConfig.refresh();
+                    return Arrays.asList(registryConfig);
+                });
+        RegistryConfig defaultRegistryConfig = registryConfigs.get(0);
 
+        if(defaultRegistryConfig.isValid()){
+            this.registryConfig = defaultRegistryConfig;
+        }else{
+            log.error("缺少配置： dubbo.registry.address, 这将会导致泛化调用失败");
+        }
     }
 }
